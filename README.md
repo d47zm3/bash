b0e88a42c291eb53c595bac07bcf800ba99e913c
Damian Tykalowski <d47zm3@gmail.com>
2020-05-28 12:29:37 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bash Framework
+
+``` bash
+source <(curl -s https://raw.githubusercontent.com/d47zm3/bash-framework/master/bash.sh)
+```
---
+++ bash.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+
+decho() {
+  string=$1
+  echo "[***] [$( date +'%H:%M:%S' )] ${string}"
+}
+
+command_exists() {
+  command_name="${1}"
+  command -v "${command_name}" &> /dev/null
+}
+
+docker_exists() {
+  container_name="${1}"
+  if [ ! "$(docker ps -aq -f name="${container_name}")" ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+ubuntu_update() {
+  apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade
+}
